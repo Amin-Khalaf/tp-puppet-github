8aad77287bc13e2bc705d074fb9b03d29a9de928
Vagrant Default User <vagrant@control>
2019-10-25 13:06:20 +0000

    install apache2

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,23 @@
+# vim: set ts=2 sw=2 et: 
 node default {
 
 file {'/etc/motd':
   path => '/etc/motd',
   content => "Helloworld via puppet ! "
-}
+  }
 }
 
 node server0 {
-package {'tmux':
-  ensure => installed,
-  name => "tmux"
+  package {'tmux':
+    ensure => installed,
+    name => "tmux"
+  }
 
- }
-
-package {'tree':
-  ensure => installed,
- } 
+  package {'tree':
+    ensure => installed,
+  } 
+  
+  package {'apache2':
+    ensure => installed,
+  }
 }
